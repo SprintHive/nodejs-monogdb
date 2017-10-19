26463f54e3a5dee06ab93f07235107e794bb2481
Jon Lai Lam <jon@lailam.co.za>
2017-10-19 14:42:34 +0200

    Finished off the finding of individual requests that fail.

--- individual.js
+++ individual.js
@@ -1,9 +1,10 @@
 const Rx = require("rxjs/Rx");
 const MongoClient = require('mongodb').MongoClient;
+const ReadPreference = require('mongodb').ReadPreference;
 const fs = require('fs-extra');
 
 const createConnection = (state) => {
-  return Rx.Observable.fromPromise(MongoClient.connect(state.url))
+  return Rx.Observable.fromPromise(MongoClient.connect(state.url, {readPreference: ReadPreference.NEAREST}))
     .map(db => {
       state.db = db;
       return state;
@@ -15,7 +16,7 @@ function findFailedIndividualResults(state) {
   const query = {profileMatches: false};
   return Rx.Observable.fromPromise(
     db.collection("IndividualVerificationResult")
-      .find(query, {limit: 60})
+      .find(query, {limit: 60, sort: {creationDate: -1}})
       .toArray())
     .map(d => {
       if (d.length) {
@@ -39,7 +40,6 @@ function findIndividualVerificationProvided(db, individualVerificationId, collec
   return Rx.Observable.fromPromise(db.collection(collection).findOne(query))
 }
 
-
 function findIndividualRequests(state) {
   const {db} = state;
   return Rx.Observable.from(Object.keys(state.results))
@@ -55,7 +55,6 @@ function findProvidedIndividualProfiles(state) {
   const {db} = state;
   return Rx.Observable.from(Object.keys(state.results))
     .mergeMap(key => findIndividualVerificationProvided(db, state.results[key].individualVerificationResult.individualVerificationId, "IndividualVerificationProvided"))
-    .do(console.log)
     .reduce((acc, r) => {
       if (acc.results[r.individualVerificationId]) acc.results[r.individualVerificationId].individualVerificationProvided = r;
       return acc;
@@ -67,9 +66,8 @@ function save(state) {
   return Rx.Observable.fromPromise(fs.writeJson('./out.json', state.results))
 }
 
-
 Rx.Observable.of({
-  url: 'mongodb://localhost:27037/dkyc-core',
+  url: 'mongodb://localhost:27047/dkyc-core',
   db: undefined, // used to store a reference to the db.
   results: {}
 }).mergeMap(createConnection)
@@ -78,6 +76,7 @@ Rx.Observable.of({
   .mergeMap(findProvidedIndividualProfiles)
   .do(save)
   .subscribe(ans => {
-    // console.log(JSON.stringify(ans.results));
+    const count = Object.keys(ans.results).length;
+    console.log(`Found ${count} records`);
     ans.db.close();
   }, err => console.log(err), () => console.log("Completed"));
