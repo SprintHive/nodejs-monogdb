761859185c6c2a210aba0aed5a7ed6445504f9e7
Jon Lai Lam <jon@lailam.co.za>
2017-10-19 14:00:26 +0200

    Finished off the finding of individual requests that fail.

--- individual.js
+++ individual.js
@@ -1,5 +1,6 @@
 const Rx = require("rxjs/Rx");
 const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs-extra');
 
 const createConnection = (state) => {
   return Rx.Observable.fromPromise(MongoClient.connect(state.url))
@@ -9,57 +10,74 @@ const createConnection = (state) => {
     })
 };
 
-function findFailedIndividualResults({state}) {
+function findFailedIndividualResults(state) {
   const {db} = state;
   const query = {profileMatches: false};
   return Rx.Observable.fromPromise(
     db.collection("IndividualVerificationResult")
-      .find(query)
+      .find(query, {limit: 60})
       .toArray())
     .map(d => {
       if (d.length) {
         d.forEach(r => {
-           state.results[r.individualVerificationId] = {
-             individualVerificationResult: r
-           }
+          state.results[r.individualVerificationId] = {
+            individualVerificationResult: r
+          }
         });
       }
       return state;
     })
 }
 
-function findIndividualRequests({state}) {
+function findById(db, _id, collection) {
+  const query = {_id};
+  return Rx.Observable.fromPromise(db.collection(collection).findOne(query))
+}
+
+function findIndividualVerificationProvided(db, individualVerificationId, collection) {
+  const query = {individualVerificationId};
+  return Rx.Observable.fromPromise(db.collection(collection).findOne(query))
+}
+
+
+function findIndividualRequests(state) {
   const {db} = state;
-  const query = {profileMatches: true};
-  return Rx.Observable.fromPromise(
-    db.collection("IndividualVerificationResult")
-      .find(query)
-      .toArray())
-    .map(d => {
-      if (d.length) {
-        d.forEach(r => {
-           state.results[r.individualVerificationId] = {
-             individualVerificationResult: r
-           }
-        });
-      }
-      return state;
-    })
+  return Rx.Observable.from(Object.keys(state.results))
+    .mergeMap(key => findById(db, state.results[key].individualVerificationResult.individualVerificationId, "IndividualVerificationRequested"))
+    .reduce((acc, r) => {
+      if (acc.results[r._id]) acc.results[r._id].individualVerificationRequested = r;
+      return acc;
+    }, state)
+    .map(() => state)
 }
 
-const eventStream = Rx.Observable.of({
-  url: 'mongodb://localhost:27017/dkyc-core',
+function findProvidedIndividualProfiles(state) {
+  const {db} = state;
+  return Rx.Observable.from(Object.keys(state.results))
+    .mergeMap(key => findIndividualVerificationProvided(db, state.results[key].individualVerificationResult.individualVerificationId, "IndividualVerificationProvided"))
+    .do(console.log)
+    .reduce((acc, r) => {
+      if (acc.results[r.individualVerificationId]) acc.results[r.individualVerificationId].individualVerificationProvided = r;
+      return acc;
+    }, state)
+    .map(() => state)
+}
+
+function save(state) {
+  return Rx.Observable.fromPromise(fs.writeJson('./out.json', state.results))
+}
+
+
+Rx.Observable.of({
+  url: 'mongodb://localhost:27037/dkyc-core',
   db: undefined, // used to store a reference to the db.
   results: {}
 }).mergeMap(createConnection)
-  .mergeMap(state => findFailedIndividualResults({state}))
-  .mergeMap(state => findIndividualRequests({state}));
-
-
-eventStream
+  .mergeMap(findFailedIndividualResults)
+  .mergeMap(findIndividualRequests)
+  .mergeMap(findProvidedIndividualProfiles)
+  .do(save)
   .subscribe(ans => {
-    console.log(JSON.stringify(ans.results));
+    // console.log(JSON.stringify(ans.results));
     ans.db.close();
   }, err => console.log(err), () => console.log("Completed"));
-
-
